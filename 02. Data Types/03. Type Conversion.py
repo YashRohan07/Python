9a400e319738b4112cf109f75e3024f6bcd1a4c1
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2024-02-12 19:45:30 +0600

    Add files via upload

---
+++ 02. Data Types/03. Type Conversion.py
@@ -0,0 +1,16 @@
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+a = float(x)    # Convert from int to float
+b = int(y)      # convert from float to int
+c = complex(x)  # convert from int to complex:
+# You cannot convert complex numbers into another number type
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
